b1e9aea0b65337fc8fa65ce0ff4c15cfc45f5e64
Laszlo Csele <laszlo.csele90@gmail.com>
2017-05-07 03:56:46 +0200

    update build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 gulp
-echo $PWD
+
 cd webapp
-echo $PWD
-npm install
-npm run ionic:build --prod
+yarn install
+yarn ionic:build --prod
 
 cd ..
-echo $PWD
 cp -R webapp/www build/
 
 node dist/index.js
