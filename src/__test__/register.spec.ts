cd1bd42f89166ed4fbcff92550423bb274e7e56e
Laszlo Csele <lacka@codingsans.com>
2017-05-14 20:01:49 +0200

    added mockgoose to test

--- src/__test__/register.spec.ts
+++ src/__test__/register.spec.ts
@@ -2,8 +2,9 @@ import { config } from '../config';
 import { databaseInit } from '../database/database';
 import { start } from '../server';
 
-before(async () => {
-  await databaseInit().then(start).then((app) => {
+before(async function () {
+  this.timeout(120000);
+  await databaseInit(true).then(start).then((app) => {
     return app.listen(config.port, config.host);
   });
 });
--- src/database/database.ts
+++ src/database/database.ts
@@ -1,10 +1,28 @@
 import * as Mongoose from 'mongoose';
+import { Mockgoose } from 'mockgoose';
 import * as bluebird from 'bluebird';
 import { config } from '../config';
 
-async function init() {
-  const baseUrl = config.env === 'test' ? 'mongodb://127.0.0.1:32769' : config.database.url;
+async function init(isTest: boolean) {
+  const baseUrl = isTest ? 'mongodb://localhost:32769' : config.database.url;
   const uri = `${baseUrl}/${config.database.collectionName}`;
+  console.log(uri)
+
+  if (isTest) {
+    const mockgoose = new Mockgoose(Mongoose); // tslint:disable-line
+
+    return mockgoose.prepareStorage().then(() => {
+      return new Promise((resolve, reject) => {
+        (Mongoose as any).Promise = bluebird;
+        Mongoose.connect(uri, (error) => {
+          if (error) {
+            reject();
+          }
+          resolve();
+        });
+      })
+    });
+  }
 
   return new Promise((resolve, reject) => {
     (Mongoose as any).Promise = bluebird;
@@ -14,12 +32,12 @@ async function init() {
       }
       resolve();
     });
-  })
+  });
 }
 
 let initPromise;
-export function databaseInit() {
+export function databaseInit(isTest = false) {
   return initPromise || (initPromise = (async () => {
-    await init();
+    await init(isTest);
   })());
 }
