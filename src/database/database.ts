21d50f9ef41db6a80381ad3eaaae10087bea7d8b
Laszlo Csele <lacka@codingsans.com>
2017-05-15 08:38:57 +0200

    added timestamps and virtuals

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,13 +3,25 @@ import { Mockgoose } from 'mockgoose';
 import * as bluebird from 'bluebird';
 import { config } from '../config';
 
+function baseOptionsPlugin (schema: Mongoose.Schema) {
+  schema.set('timestamps', true);
+  schema.set('versionKey', false);
+  schema.set('toJSON', {
+    virtuals: true,
+  });
+  schema.set('toObject', {
+    virtuals: true,
+  });
+}
+
+Mongoose.plugin(baseOptionsPlugin);
+
 async function init(isTest: boolean) {
   const baseUrl = isTest ? 'mongodb://localhost:32769' : config.database.url;
   const uri = `${baseUrl}/${config.database.collectionName}`;
-  console.log(uri)
 
   if (isTest) {
-    const mockgoose = new Mockgoose(Mongoose); // tslint:disable-line
+    const mockgoose = new Mockgoose(Mongoose);
 
     return mockgoose.prepareStorage().then(() => {
       return new Promise((resolve, reject) => {
